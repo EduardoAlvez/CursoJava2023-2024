abe93e452a404cc455dc83b27970454bc1f487ce
eduardo.alves <eduardo.alves@LAB3-DCX-26>
2023-12-12 15:07:39 -0300

    Set, HasSet, SortedSet.

--- Desktop/CursoJava/CursoJava/src/Colecoes/ConjuntoBaguncado.java
+++ Desktop/CursoJava/CursoJava/src/Colecoes/ConjuntoBaguncado.java
@@ -1,8 +1,10 @@
 package Colecoes;
 
 import java.util.HashSet;
+import java.util.Set;
 
 public class ConjuntoBaguncado {
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public static void main(String[] args) {
 
 		HashSet conjunto = new HashSet();
@@ -21,6 +23,25 @@ public class ConjuntoBaguncado {
 		System.out.println(conjunto.remove("teste"));
 		System.out.println(conjunto.remove("Teste"));
 		
+		System.out.println("Tem? "+conjunto.contains('x'));
+		
+		Set nums = new HashSet(); // FUNCIONA COMO HASHSET, POREM O SET PARECE SER UMA INTERFACE...;
+		
+		nums.add(1);
+		nums.add(2);
+		nums.add(3);
+		System.out.println(nums);
+		System.out.println(conjunto);
+		
+		conjunto.addAll(nums); // Uniao entre dois conjunto
+		System.out.println(conjunto);
+		System.out.println();
+		
+		conjunto.retainAll(nums); // retira os dados idem
+		System.out.println(conjunto);
+		
+		conjunto.clear(); // limpa os dados
+		System.out.println(conjunto);
 		
 	}
 
