1cdeb7886c016747b2222b3d315ece17ada530b8
eduardo.alves <eduardo.alves@LAB3-DCX-26>
2023-12-20 20:41:27 -0300

    Up

--- Desktop/CursoJava/CursoJava/src/lambdas/Foreach.java
+++ Desktop/CursoJava/CursoJava/src/lambdas/Foreach.java
@@ -7,7 +7,7 @@ public class Foreach {
 
 	public static void main(String[] args) {
 		List<String> aprovados = 
-				Arrays.asList("Ivone", "Fernanda", "Eduardo","Ibison");
+				Arrays.asList("Momo","Momo");
 	
 		System.out.println("Forma tradicional...");
 		for (String nome : aprovados) {
@@ -17,8 +17,18 @@ public class Foreach {
 		System.out.println("\nLAMBDA LAMBDA LAMBDA NEEERD.. #1\n");
 		aprovados.forEach(nome -> System.out.println(nome+" TOMAA SANTANAS."));
 		
-		System.out.println("\nMethod Reference...");
+		System.out.println("\nMethod Reference...#1");
 		aprovados.forEach(System.out::println);
 				
+		System.out.println("\nLAMBDA LAMBDA LAMBDA NEEERD.. #2\n");
+		aprovados.forEach(nome -> meuImprimir(nome));
+	
+
+		System.out.println("\nMethod Reference...#2");
+		aprovados.forEach(Foreach::meuImprimir);
+	}
+	
+	static void meuImprimir(String nome) {
+		System.out.println("Oi! Meu nome é " + nome + " de " + nome);
 	}
 }
